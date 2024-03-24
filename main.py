01eec403709d5cc79afdf64213baa68440a43af0
Genaro Nadile <genaronadile@gmail.com>
2024-03-24 18:28:31 -0300

    Added a mix that only uses ANTS for validation and a reduce number of instances of the images to avoid slope

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from processors.ANTS.yolo import process_ANTS
 from processors.roboflow.yolo import process_roboflow
 from generators.mix_roboflow_ANTS import generate_roboflow_ANTS_mix
+from generators.mix_roboflow_ANTS_reduced_slope import generate_roboflow_ANTS_mix_reduced_slope
 if __name__ == "__main__":
     print("---Hello and welcome to oso-hormiguero's dataset processor and mix generator---")
     print("-------------------------------------------------------------------------------")
@@ -19,9 +20,16 @@ option: """)
             else:
                 process_roboflow("./results/datasets")
         else:
-            option_2 = input("""1. Mix for train40""")
-            print("------------------------------------------------")
-            generate_roboflow_ANTS_mix()
-            print("------------------------------------------------")
-            
+            option_2 = input("""1. Mix for train40
+2. Mix for train40 with less slope
+option: """)
+            if option_2 == "1":
+                print("------------------------------------------------")
+                generate_roboflow_ANTS_mix()
+                print("------------------------------------------------")
+            else:
+                print("------------------------------------------------")
+                generate_roboflow_ANTS_mix_reduced_slope()
+                print("------------------------------------------------")
+                
     
